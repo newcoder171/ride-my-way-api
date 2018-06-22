fba616cdb35de88d3441c201dc175efe8090b028
newcoder <newcoder17@gmail.com>
2018-06-22 18:24:48 +0100

    initial setup

---
+++ test/test.js
@@ -0,0 +1,15 @@
+var should = require('should');
+var request = require('request');
+var expect = require('chai').expect;
+
+// Some sample test I found in the documentation
+
+var assert = require('assert');
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+    });
+});
+
