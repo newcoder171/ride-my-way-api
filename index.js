a498f4f184c5656f597f480bf97a5e34a67dbee5
newcoder <newcoder17@gmail.com>
2018-06-25 07:26:21 +0100

    index update

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+//import express from 'express';
+//import bodyParser from 'body-parser'
+const bodyParser = require('body-parser')
+const app = express();
+app.use(bodyParser.json())
+
+require('./app/routes/rides.routes.js')(app);
+
+// Create a Server
+const port = 8081;
+app.listen(port, () => {
+    console.log(`App listening at ${port}`)
+
+})
