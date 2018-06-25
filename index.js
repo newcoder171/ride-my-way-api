ce7397ec9f5acf14d0e078ef1dcd21b6c6900ac3
newcoder <newcoder17@gmail.com>
2018-06-25 18:04:38 +0100

    update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 require('./route/rides.routes.js')(app);
 
 // Create a Server
-const port = 8081;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App listening at ${port}`)
 
