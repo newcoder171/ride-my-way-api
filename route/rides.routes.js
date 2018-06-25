29cac7bac8b770acaad22e4e2b60233ea0eaaef8
newcoder <newcoder17@gmail.com>
2018-06-25 13:25:40 +0100

    add Procfile

--- route/rides.routes.js
+++ route/rides.routes.js
@@ -1,22 +1,22 @@
 module.exports = function (app) {
 
-    const rides = require('../controller/rides.controller.js');
+  const rides = require('../controller/rides.controller.js');
 
-    // Create a new Customer
-    app.post('/api/v1/rides', rides.create);
+  // Create a new Customer
+  app.post('/api/v1/rides', rides.create);
 
-    // Retrieve all Customer
-    app.get('/api/v1/rides', rides.findAll);
+  // Retrieve all Customer
+  app.get('/api/v1/rides', rides.findAll);
 
-    // Retrieve a single Customer by Id
-    app.get('/api/v1/rides/:id', rides.findOne);
+  // Retrieve a single Customer by Id
+  app.get('/api/v1/rides/:id', rides.findOne);
 
-    // Update a Customer with Id
-    app.put('/api/v1/rides/:id', rides.update);
+  // Update a Customer with Id
+  app.put('/api/v1/rides/:id', rides.update);
 
-    // Delete a Customer with Id
-    app.delete('/api/v1/rides/:id', rides.delete);
+  // Delete a Customer with Id
+  app.delete('/api/v1/rides/:id', rides.delete);
 
-    // Request to join ride of specific Id
-    app.put('/api/v1/rides/:id/requests', rides.update);
-}
+  // Request to join ride of specific Id
+  app.put('/api/v1/rides/:id/requests', rides.update);
+};
